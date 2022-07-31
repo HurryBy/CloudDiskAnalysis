6a53b9e88c863cc5d28c8503bb2e55dc4c8c1972
HurryBy <2498572743@qq.com>
2022-07-31 14:20:31 +0800

    删除不必要的代码
    删除不必要的代码

--- 网页版/id.php
+++
@@ -1,6 +0,0 @@
-<?php
-$link = isset($_GET['link']) ? $_GET['link'] : NULL;
-$length = strlen($link)-strrpos($link,"/")+1;
-$lanzou_id = substr($link,strrpos($link,"/")+1,$length);
-echo $lanzou_id;
-?>
