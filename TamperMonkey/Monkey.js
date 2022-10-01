5890989f343bd4e58e24cc6fde42e3a368b63960
Hurry <47547391+HurryBy@users.noreply.github.com>
2022-10-01 16:24:15 +0800

    JavaScript版本
    JavaScript版本 不是油猴脚本

--- TamperMonkey/Monkey.js
+++ TamperMonkey/Monkey.js
@@ -1,7 +1,7 @@
-var HtmlMessage = document.documentElement.outerHTML;
+var HtmlMessage = document.getElementsByTagName("body")[0];
 const regex = /((?:https?:\/\/)?(?:[a-zA-Z0-9\-.]+)?lanzou[a-z]\.com\/[a-zA-Z0-9_\-]+)/g;
 var link = '';
-while ((m = regex.exec(HtmlMessage)) !== null) {
+while ((m = regex.exec(HtmlMessage.outerHTML)) !== null) {
     if (m.index === regex.lastIndex) {
         regex.lastIndex++;
     }
@@ -11,7 +11,15 @@ while ((m = regex.exec(HtmlMessage)) !== null) {
 		xhr.onreadystatechange = function(){
 		if (xhr.readyState === 4){
 			if (xhr.status === 200){
-                    console.log(xhr.responseText)
+                    var text = xhr.responseText;
+					obj = JSON.parse(text);
+					if(obj.msg == "解析成功"){
+						url = obj.data.url;
+						HtmlMessage.outerHTML = HtmlMessage.outerHTML.toString().replace(`${match}`,`${match} <a href=` + url + ">[Hurry]一键下载</a>");
+					}else{
+						HtmlMessage.outerHTML = HtmlMessage.outerHTML.toString().replace(`${match}`,`${match} <a href=` + url + ">[Hurry]一键下载(未获取到下载链接)</a>");
+					}
+					
 				}
 				console.error(xhr.statusText);
 			}
@@ -19,10 +27,7 @@ while ((m = regex.exec(HtmlMessage)) !== null) {
 		xhr.onerror = function (e) {
 		    console.error(xhr.statusText);
 		};
-		xhr.open('GET', `https://wgame.miaovps.com:32022/api/api.php?link=${matches}&red=`, true);
+		xhr.open('GET', `https://lanzou.humorously.tk/api/api.php?link=${match}&red=`, true);
 		xhr.send(null);
     });
 }
-
-
-
